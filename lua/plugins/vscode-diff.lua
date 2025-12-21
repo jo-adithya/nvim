5f74fa84d69963092ee4ed4873f81194ca53aec3
Jonathan Adithya <joadithya39@gmail.com>
2025-12-21 09:41:19 -0800

    chore: comment vscode diff for now

--- lua/plugins/vscode-diff.lua
+++ lua/plugins/vscode-diff.lua
@@ -1,10 +1,10 @@
-MiniDeps.add({
-	source = "esmuellert/vscode-diff.nvim",
-	depends = { "MunifTanjim/nui.nvim" },
-})
-
-function setup()
-	require("vscode-diff").setup({})
-end
-
-MiniDeps.later(setup)
+-- MiniDeps.add({
+-- 	source = "esmuellert/vscode-diff.nvim",
+-- 	depends = { "MunifTanjim/nui.nvim" },
+-- })
+--
+-- function setup()
+-- 	require("vscode-diff").setup({})
+-- end
+--
+-- MiniDeps.later(setup)
