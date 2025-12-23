c234c9debec3123e5a6ab62fa3dfa33226387798
Jonathan Adithya <joadithya39@gmail.com>
2025-12-23 11:17:41 -0800

    fix: diagnostics

--- lua/config/diagnostics.lua
+++ lua/config/diagnostics.lua
@@ -1,18 +1,30 @@
 vim.diagnostic.config({
-  virtual_lines = { current_line = true },
-  underline = true,
-  update_in_insert = true,
-  severity_sort = true,
-  signs = {
-    text = {
-      [vim.diagnostic.severity.ERROR] = " ",
-      [vim.diagnostic.severity.WARN] = " ",
-      [vim.diagnostic.severity.INFO] = " ",
-      [vim.diagnostic.severity.HINT] = " ",
-    },
-  },
-  float = {
-    border = "rounded",
-    source = "if_many",
-  },
+	virtual_text = {
+		source = "if_many",
+	   current_line = false,
+		prefix = function(_diagnostic, index, _total)
+			return index == 1 and " " or ""
+		end,
+	},
+	underline = true,
+	update_in_insert = true,
+	severity_sort = true,
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = " ",
+			[vim.diagnostic.severity.WARN] = " ",
+			[vim.diagnostic.severity.INFO] = " ",
+			[vim.diagnostic.severity.HINT] = " ",
+		},
+		numhl = {
+			[vim.diagnostic.severity.ERROR] = "DiagnosticVirtualTextError",
+			[vim.diagnostic.severity.WARN] = "DiagnosticVirtualTextWarn",
+			[vim.diagnostic.severity.INFO] = "DiagnosticVirtualTextInfo",
+			[vim.diagnostic.severity.HINT] = "DiagnosticVirtualTextHint",
+		},
+	},
+	float = {
+		border = "rounded",
+		source = "if_many",
+	},
 })
