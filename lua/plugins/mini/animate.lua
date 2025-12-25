cdfca8513bb3780b604bf6a96674a95f27bd399b
Jonathan Adithya <joadithya39@gmail.com>
2025-12-24 20:13:22 -0800

    wip

--- lua/plugins/mini/animate.lua
+++
@@ -1,7 +0,0 @@
-MiniDeps.add('nvim-mini/mini.animate')
-
-function setup()
-  -- require('mini.animate').setup()
-end
-
-MiniDeps.later(setup)
--- lua/plugins/mini/git.lua
+++
@@ -1,7 +0,0 @@
-MiniDeps.add("nvim-mini/mini-git")
-
-function setup()
-	require("mini.git").setup()
-end
-
-MiniDeps.later(setup)
